2c13cb2a70ff53f05aa591d1e9414fa3110140cc
Dovand <Dovand.11@gmail.com>
2026-07-20 03:25:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
-// PAKAI INI KALAU DI VERCEL
 const SUPABASE_URL = 'https://hmshfssjxudjllfgohal.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhtc2hmc3NqeHVkamxsZmdvaGFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ0Njc3NjEsImV4cCI6MjEwMDA0Mzc2MX0.4lt3iRVD9ktBUiatDk6DCJlSV1FAwQ1p5qPCwex31PM';
 
-// CEK APAKAH SUPABASE SUDAH LOAD
+// CEK SUPABASE JS SUDAH LOAD
 if (typeof supabase === 'undefined') {
-    console.error('❌ Supabase JS belum di-load!');
+    console.error('❌ Supabase JS tidak ditemukan! Cek CDN.');
 } else {
     const sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
     console.log('✅ Supabase connected');
